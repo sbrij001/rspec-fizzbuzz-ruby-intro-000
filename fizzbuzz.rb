510706b92a13734843290cf7632c69aaab144a13
shivani brijmohan <sbrij001@yahoo.com>
2018-11-20 21:35:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,12 @@
 
 
 def fizzbuzz(x)
-
+  if x % 3 == 0 
+    return "Fizz"
+  elsif  x % 5 == 0 
+    return "Buzz"
+  elsif (x % 3 == 0 && x % 5 == 0)
+    return "FizzBuzz"
+  else
+    return nil 
 end

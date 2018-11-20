c3c8d6c43cdcd618db0af9d763453761f4b5cb04
shivani brijmohan <sbrij001@yahoo.com>
2018-11-20 21:40:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 
 x = 3
 def fizzbuzz(x)

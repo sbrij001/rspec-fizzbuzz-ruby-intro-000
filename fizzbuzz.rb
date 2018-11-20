1ef3a1fa1fd12a132d1cfda93f819ff71d972d4b
shivani brijmohan <sbrij001@yahoo.com>
2018-11-20 21:46:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
 
 x = 3
 def fizzbuzz(x)
 
   if x % 3 == 0
     print "Fizz"
-  elsif  x % 5 == 0
-    print "Buzz"
-  elsif (x % 3 == 0 && x % 5 == 0)
-    print "FizzBuzz"
   else
     print nil
   end

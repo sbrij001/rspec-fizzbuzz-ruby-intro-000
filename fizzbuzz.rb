d80e573362bb6bc4eebc8fc77d34c0409c9dcbb2
shivani brijmohan <sbrij001@yahoo.com>
2018-11-20 21:39:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 
 def fizzbuzz(x)
 
-  if x / 3 == 0
+  if x % 3 == 0
     puts "Fizz"
-  elsif  x / 5 == 0
+  elsif  x % 5 == 0
     puts "Buzz"
-  elsif (x / 3 == 0 && x / 5 == 0)
+  elsif (x % 3 == 0 && x % 5 == 0)
     puts "FizzBuzz"
   else
     puts nil
